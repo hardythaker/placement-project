50a3a27cf4968fbb3f76b2450a08bc386d5acf07
hardythaker <hardik.thaker1994@gmail.com>
2016-03-14 20:26:26 +0530

    Added Branch Manager page

---
+++ PlacementCell/AdminEditorTools/js/branchManager.js
@@ -0,0 +1,59 @@
+﻿(function ($) {
+    $(document).ready(function () {
+        var editor = new $.fn.dataTable.Editor({
+            ajax: '/api/BranchManager',
+            table: '#BranchTable',
+            fields: [
+                {
+                    'label': "Branch Name :",
+                    'name': 'branch_name'
+                }
+            ]
+        });
+
+        var table = $('#BranchTable').DataTable({
+            ajax: {
+                url: '/api/BranchManager',
+                type: 'POST'
+            },
+            processing: true,
+            'columns': [
+                { data: null },
+                { 'data': 'branch_name' }
+            ],
+            select: {
+                style: 'multi',
+                blurable: true,
+            },
+            dom: 'Bfrtip',
+            buttons: [
+                { extend: 'create', editor: editor },
+                { extend: 'edit', editor: editor },
+                { extend: 'remove', editor: editor },
+            ],
+            language: {
+                select: {
+                    rows: {
+                        _: "<b> %d Rows Selected</b>",
+                        0: "",
+                    }
+                },
+                "loadingRecords": "Please Wait  - Loading...",
+                "processing": "<span id='loader' class='mdl-spinner mdl-js-spinner is-active'></span>"
+            },
+            "columndefs": [
+                {
+                    "searchable": false,
+                    "orderable": false,
+                    "targets": 0
+                }
+            ],
+            "order": [[1, 'asc']],
+        });
+        table.on('order.dt search.dt', function () {
+            table.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
+                cell.innerHTML = i + 1;
+            });
+        }).draw();
+    });
+}(jQuery));
---
+++ PlacementCell/Controllers/BranchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using DataTables;
+using PlacementCell.Models;
+using System.Web;
+using System.Web.Http;
+
+namespace PlacementCell.Controllers
+{
+    public class BranchController : ApiController
+    {
+        [Route("api/BranchManager")]
+        [HttpGet]
+        [HttpPost]
+        public IHttpActionResult BranchData()
+        {
+            var request = HttpContext.Current.Request;
+            var settings = Properties.Settings.Default;
+
+            using (var db = new Database(settings.DbType, settings.DbConnection))
+            {
+                var response = new Editor(db, "branch", "branch_id")
+                    .Model<BranchModel>()
+                    .Field(new Field("branch_name"))
+                    .Process(request)
+                    .Data();
+                return Json(response);
+            }
+        }
+    }
+}
